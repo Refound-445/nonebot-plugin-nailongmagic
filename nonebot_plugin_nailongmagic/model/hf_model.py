ca8bec93d899d8582633546e7d7e4542f3e55c05
Refound <137168144+Refound-445@users.noreply.github.com>
2024-12-08 14:48:38 +0800

    Update hf_model.py

--- nonebot_plugin_nailongmagic/model/hf_model.py
+++ nonebot_plugin_nailongmagic/model/hf_model.py
@@ -6,7 +6,8 @@ from diffusers import AutoPipelineForImage2Image
 from PIL import Image as PILImage
 
 from ..config import config
-
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 token = config.nailongmagic_hf_token
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -14,15 +15,15 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 pipeline = AutoPipelineForImage2Image.from_pretrained(
     "stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16 if torch.cuda.is_available() else None, variant="fp16", use_safetensors=True,
-    cache_dir=config.nailongmagic_cache_dir,token=token,device_map="cpu"
+    cache_dir=config.nailongmagic_cache_dir,token=token
 ).to(device)
 
 
 pipeline.load_lora_weights("refoundd/NailongT", weight_name="pytorch_lora_weights.safetensors", adapter_name="nailong",
-                           cache_dir=config.nailongmagic_cache_dir,token=token,device_map="cpu")
+                           cache_dir=config.nailongmagic_cache_dir,token=token)
 
-
-pipeline.enable_model_cpu_offload()
+if torch.cuda.is_available():
+    pipeline.enable_model_cpu_offload()
 
 
 # remove following line if xFormers is not installed or you have PyTorch 2.0 or higher installed
