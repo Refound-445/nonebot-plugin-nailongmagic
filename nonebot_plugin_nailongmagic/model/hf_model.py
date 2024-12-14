3fdf4f3faa96c0b3151bd192c234b59b53e9ef27
Refound <137168144+Refound-445@users.noreply.github.com>
2024-12-14 23:19:23 +0800

    Update hf_model.py

--- nonebot_plugin_nailongmagic/model/hf_model.py
+++ nonebot_plugin_nailongmagic/model/hf_model.py
@@ -11,16 +11,16 @@ from ..config import config
 
 token = config.nailongmagic_hf_token
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+cache_dir = store.get_plugin_cache_dir() / "data/nailongmagic"
 
 pipeline = AutoPipelineForImage2Image.from_pretrained(
     "stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16 if torch.cuda.is_available() else None, variant="fp16", use_safetensors=True,
-    cache_dir=config.nailongmagic_cache_dir,token=token
+    cache_dir=cache_dir,token=token
 ).to(device)
 
 
 pipeline.load_lora_weights("refoundd/NailongT", weight_name="pytorch_lora_weights.safetensors", adapter_name="nailong",
-                           cache_dir=config.nailongmagic_cache_dir,token=token)
+                           cache_dir=cache_dir,token=token)
 
 if torch.cuda.is_available():
     pipeline.enable_model_cpu_offload()
